5323f3b34c24463d433cd31242640558c972bbf3
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2026-02-06 04:41:24 +0600

    Get single user

--- src/server.ts
+++ src/server.ts
@@ -75,11 +75,11 @@ app.get("/users", async (req: Request, res: Response) => {
     const query = `SELECT * FROM users`;
     const result = await pool.query(query);
 
-    res.status(201).json({
-      success: true,
-      message: "Data Fetch successfully!",
-      data: result.rows,
-    });
+    res.status(200).json({
+        success: true,
+        message: "Data Fetch successfully!",
+        data: result.rows,
+      });
   } catch (error: any) {
     res.status(500).json({
       success: false,
@@ -87,6 +87,33 @@ app.get("/users", async (req: Request, res: Response) => {
     });
   }
 });
+
+
+app.get('/users/:id', async(req:Request, res:Response)=>{
+    try {
+        const {id} = req.params;
+        const query = `SELECT * FROM users WHERE id=${id}`
+        const result = await pool.query(query);
+
+        if(result.rows.length <= 0){
+            return res.status(400).json({
+                success: false,
+                message: "User Not Found",
+              });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Data Fetch successfully!",
+            data: result.rows,
+          });
+    } catch (error:any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+          });
+    }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
